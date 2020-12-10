81530e6066d5bcddab1da19dc707c7b4aa5d6b62
Полина <lovepolina2004@gmail.com>
2020-12-10 19:50:10 +0300

    yo

---
+++ yel/yellow circle.py
@@ -0,0 +1,39 @@
+import sys
+
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+        self.setWindowTitle('Жёлтый круг')
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x, y, r = randint(0, 800), randint(0, 600), randint(1, 600)
+        qp.drawEllipse(x - r // 2, y - r // 2, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
